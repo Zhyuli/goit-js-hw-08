473cda717d7489c1d2474fb4051a211606628f4c
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2023-11-20 11:58:46 +0200

    install simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const container = document.querySelector('.gallery');
+// // console.log(container);
+const createMarkup = createGalMarkup(galleryItems);
+
+container.insertAdjacentHTML('beforeend', createMarkup);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250});
+
+function createGalMarkup(items) {
+    return items.map(({ original, preview, description }) =>
+   `<li class="gallery__item">
+        <a class="galllery__link" href="${original}">
+        <img
+         class="gallery__image"
+         src="${preview}"
+         data-source="${original}"
+         alt="${description}"
+
+        />
+ </a>
+</li>`
+        )
+        .join('')
+}
+
 
 console.log(galleryItems);
