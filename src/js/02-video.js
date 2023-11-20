20ef174b409b0b49380a2ecad0e234db30770f4e
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2023-11-20 21:15:05 +0200

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,58 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+const onPlay = function (data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+}
+player.on('play', throttle(onPlay, 1000));
+
+const currentTime = +localStorage.getItem("videoplayer-current-time");
+console.log(currentTime);
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
+    // player.on('play', function() {
+    //     console.log('played the video!');
+    // });
+
+    // player.getVideoTitle().then(function(title) {
+    //     console.log('title:', title);
+// });
+
+// * on(event: string, callback: function): void
+// const onPlay = function(data) {
+//     // data is an object containing properties specific to that event
+// };
+
+// player.on('play', onPlay);
+
+//  *setCurrentTime(seconds: number): Promise<number, (RangeError|Error)>
+
+//     player.setCurrentTime(30.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
